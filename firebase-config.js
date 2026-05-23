33092a67102eb1a3cc4db449a23c6594011c3e9c
jonahstaub <jonahstaub@gmail.com>
2026-05-23 07:40:03 -0700

    chore: configure firebase sync

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,9 @@
-window.SCHOOL_WIKI_FIREBASE_CONFIG = null;
-
-// Paste your Firebase web app config here when you create a Firebase project.
-// Example:
-// window.SCHOOL_WIKI_FIREBASE_CONFIG = {
-//   apiKey: "...",
-//   authDomain: "...",
-//   databaseURL: "https://YOUR-PROJECT-default-rtdb.firebaseio.com",
-//   projectId: "...",
-//   storageBucket: "...",
-//   messagingSenderId: "...",
-//   appId: "..."
-// };
+window.SCHOOL_WIKI_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBKlu0lXkoIWmcUp4pYX9GtONzzE27_SH8",
+  authDomain: "school-wiki-ac8c5.firebaseapp.com",
+  databaseURL: "https://school-wiki-ac8c5-default-rtdb.firebaseio.com",
+  projectId: "school-wiki-ac8c5",
+  storageBucket: "school-wiki-ac8c5.firebasestorage.app",
+  messagingSenderId: "146839574396",
+  appId: "1:146839574396:web:cf4c4d31b81950af2db6f9",
+};
